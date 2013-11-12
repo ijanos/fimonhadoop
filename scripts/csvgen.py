40657a11c696b44a70e762395dfee48963dac948
János Illés <janos.illes@aut.bme.hu>
2013-11-12 15:36:09 +0100

    Python script for generating csv files

---
+++ scripts/csvgen.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+INPUTS = [
+    ("/hduser/apriori/10m", 10000000),
+    ("/hduser/apriori/20m", 20000000)
+    ]
+
+MINSUPS = [0.05, 0.01, 0.005]
+
+if __name__ == "__main__":
+    id = 1
+    for minsup in MINSUPS:
+        for path, size in INPUTS:
+            print(",".join([str(x) for x in (id, path, size, minsup)]))
+            id += 1
