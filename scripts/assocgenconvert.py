f1b0d389f57a16473c0c4c188dd372b8b982e1d9
János Illés <janos.illes@aut.bme.hu>
2013-12-10 18:34:19 +0100

    Converter script from the IBM assocgen ASCII fromat

---
+++ scripts/assocgenconvert.py
@@ -0,0 +1,21 @@
+import sys
+
+
+curr_items = []
+curr_transid = None
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        segments = line.strip().split()
+        trid = segments[0]
+        item = segments[2]
+        if trid != curr_transid:
+            if curr_transid is not None:
+                print(curr_transid, " ".join(curr_items))
+            curr_transid = trid
+            curr_items = [item]
+        else:
+            curr_items.append(item)
+
+    # flush last transaction
+    print(curr_transid, " ".join(curr_items))
