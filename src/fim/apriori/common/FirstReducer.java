6296328c772b262069ab271928a176df0532c5e8
János Illés <janos.illes@aut.bme.hu>
2013-12-17 18:04:29 +0100

    Remove unnecessary string array.

--- src/fim/apriori/common/FirstReducer.java
+++ src/fim/apriori/common/FirstReducer.java
@@ -3,7 +3,6 @@ package fim.apriori.common;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -21,10 +20,9 @@ public class FirstReducer extends Reducer<Text, IntWritable, Text, NullWritable>
 
 	private static final Log LOG = LogFactory.getLog(FirstReducer.class);
 
-	private final Pattern space = Pattern.compile(" ");
 	private int minsup;
 	private int iteration;
-	private List<String[]> largeItemsets;
+	private List<String> largeItems;
 	private List<String[]> candidateItems;
 	private boolean profile;
 	private long reduceStart;
@@ -53,7 +51,7 @@ public class FirstReducer extends Reducer<Text, IntWritable, Text, NullWritable>
 			throw new IOException("Called in the wrong iteration");
 		}
 
-		largeItemsets = new ArrayList<String[]>();
+		largeItems = new ArrayList<String>();
 		LOG.info("Starting Apriori reducer first iteration. Minum support: " + minsup);
 
 		reduceStart = System.nanoTime();
@@ -66,8 +64,9 @@ public class FirstReducer extends Reducer<Text, IntWritable, Text, NullWritable>
 		for (final IntWritable count : counts) {
 			sumCount += count.get();
 		}
+
 		if (sumCount > minsup) {
-			largeItemsets.add(space.split(itemset.toString().trim()));
+			largeItems.add(itemset.toString().trim());
 		}
 		context.progress();
 
@@ -82,12 +81,12 @@ public class FirstReducer extends Reducer<Text, IntWritable, Text, NullWritable>
 		// Generate k+1 candidate itemsets
 		candidateItems = new ArrayList<String[]>();
 
-		for (int i = 0; i < largeItemsets.size(); i++) {
-			for (int j = i + 1; j < largeItemsets.size(); j++) {
-				if (Integer.valueOf(largeItemsets.get(i)[0]) < Integer.valueOf(largeItemsets.get(j)[0])) {
-					candidateItems.add(new String[] { largeItemsets.get(i)[0], largeItemsets.get(j)[0] });
+		for (int i = 0; i < largeItems.size(); i++) {
+			for (int j = i + 1; j < largeItems.size(); j++) {
+				if (Integer.parseInt(largeItems.get(i)) < Integer.parseInt(largeItems.get(j))) {
+					candidateItems.add(new String[] { largeItems.get(i), largeItems.get(j) });
 				} else {
-					candidateItems.add(new String[] { largeItemsets.get(j)[0], largeItemsets.get(i)[0] });
+					candidateItems.add(new String[] { largeItems.get(j), largeItems.get(i) });
 				}
 			}
 		}
